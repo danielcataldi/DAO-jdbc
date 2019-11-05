9b0ff38bd0abfb18b456ba1f4dc3916ef91417a7
Daniel <danielcataldi95@gmail.com>
2019-11-05 10:54:49 -0300

    DAOFactory e SellerDAO

--- src/app/Program.java
+++ src/app/Program.java
@@ -2,6 +2,8 @@ package app;
 
 import java.util.Date;
 
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
 		System.out.println(obj);
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.0, obj);
+		
+		SellerDAO sellerDao = DAOFactory.createSellerDAO();
 
 		System.out.println(seller);
 	}
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,42 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDAO;
+import model.entities.Department;
+
+public class SellerDaoJDBC implements SellerDAO {
+
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
